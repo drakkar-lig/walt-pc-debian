62e2ee7d69558275c3917f4248cabd215679f24f
Etienne Dublé <etienne.duble@imag.fr>
2021-03-26 17:15:43 +0100

    initramfs-tools: specify git commit.

--- build-initramfs.sh
+++ build-initramfs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 INITRAMFS_TOOLS_REPO="https://gricad-gitlab.univ-grenoble-alpes.fr/dublee/initramfs-tools"
+INITRAMFS_TOOLS_COMMIT="795614cb2a221e2f3011acf9507e33916a3a46a5"
 PACKAGES="make gcc dpkg-dev debhelper git bash-completion shellcheck"
 
 # install packages
@@ -10,5 +11,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y $PACKAGES
 cd /root
 git clone -b nbfs $INITRAMFS_TOOLS_REPO
 cd initramfs-tools
+git checkout $INITRAMFS_TOOLS_COMMIT
 dpkg-buildpackage --no-sign
 
